3a65c0b8ed5cc8912a1101fd0182fad069eb3b45
Hochin Chang <ho-chin.chang@cwa.gov.tw>
2026-05-27 14:06:36 +0800

    [Fix] Fix some shell script for deploy.

--- deploy/run_docker_nginx.sh
+++ deploy/run_docker_nginx.sh
@@ -1,5 +1,7 @@
-docker rm -f $( docker container ls|grep nginx|awk '{print $1}')
-sleep 10
-docker run -d   --name nginx  --add-host=host.docker.internal:host-gateway -p 3000:80 -v /data/b615/radar-monitoring-platform/frontend:/usr/share/nginx/html:z --restart=always nginx
+if docker container ls|grep nginx|awk '{print $1}' ;then
+   docker rm -f $( docker container ls|gr;ep nginx|awk '{print $1}')
+   sleep 3
+fi
+docker run -d --name nginx  --add-host=host.docker.internal:host-gateway -p 3000:80 -v /data/monitor/radar-monitoring-platform/frontend:/usr/share/nginx/html:z --restart=always nginx
 docker cp /data/monitor/radar-monitoring-platform/deploy/nginx.conf nginx:/etc/nginx/conf.d/default.conf
 docker exec nginx nginx -t && docker exec nginx nginx -s reload
--- deploy/run_uvicorn.sh
+++ deploy/run_uvicorn.sh
@@ -1,6 +1,7 @@
-#kill -9 $( ps -ef |grep uvicorn |grep monitor|grep -v grep|awk '{print $2}' )
-#sleep 5
+if ps -ef |grep backend |grep monitor|grep -v grep ;then
+  kill -9 $( ps -ef |grep backend |grep monitor|grep -v grep|awk '{print $2}' )
+  sleep 5
+fi
 cd ../
-echo $PWD
 nohup uvicorn backend.main:app --host 0.0.0.0 --port 8000 > logs/uvicorn.log 2>&1 &
 
