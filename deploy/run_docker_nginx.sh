ba419f8a616c388400be37c19db6270b503f0e0d
Hochin Chang <hochin@cwb.gov.tw>
2026-05-27 14:29:42 +0800

    [Branch] First Commit From b615

--- deploy/run_docker_nginx.sh
+++ deploy/run_docker_nginx.sh
@@ -2,6 +2,6 @@ if docker container ls|grep nginx|awk '{print $1}' ;then
    docker rm -f $( docker container ls|grep nginx|awk '{print $1}')
    sleep 3
 fi
-docker run -d --name nginx  --add-host=host.docker.internal:host-gateway -p 3000:80 -v /data/monitor/radar-monitoring-platform/frontend:/usr/share/nginx/html:z --restart=always nginx
+docker run -d --name nginx  --add-host=host.docker.internal:host-gateway -p 3001:80 -v /data/monitor/radar-monitoring-platform/frontend:/usr/share/nginx/html:z --restart=always nginx
 docker cp /data/monitor/radar-monitoring-platform/deploy/nginx.conf nginx:/etc/nginx/conf.d/default.conf
 docker exec nginx nginx -t && docker exec nginx nginx -s reload
