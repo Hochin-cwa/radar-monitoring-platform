2c6f6249c93e737d8d267e3c7310812d1a713135
Hochin Chang <ho-chin.chang@cwa.gov.tw>
2026-05-27 14:08:24 +0800

    [Fix] fix run_docker_nginx.sh

--- deploy/run_docker_nginx.sh
+++ deploy/run_docker_nginx.sh
@@ -1,5 +1,5 @@
 if docker container ls|grep nginx|awk '{print $1}' ;then
-   docker rm -f $( docker container ls|gr;ep nginx|awk '{print $1}')
+   docker rm -f $( docker container ls|grep nginx|awk '{print $1}')
    sleep 3
 fi
 docker run -d --name nginx  --add-host=host.docker.internal:host-gateway -p 3000:80 -v /data/monitor/radar-monitoring-platform/frontend:/usr/share/nginx/html:z --restart=always nginx
